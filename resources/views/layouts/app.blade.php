3a62e06ece4da8c4f0d8ca37005408a745a7b8a8
OgistDipen <ogistdipen@outlook.com>
2018-12-24 19:35:26 +0100

    added swiper.js and aranged front page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,72 @@
-<!DOCTYPE <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Movie Site</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="http://localhost:8000/css/swiper.min.css"/>
     <link rel="stylesheet" type="text/css" media="screen" href="http://localhost:8000/css/main.css"/>
     <script src="main.js"></script>
 </head>
 <body>
 
+<nav class="mainNavbar">
+
+    <ul class="mainLogo">
+        <li><img src="http://localhost:8000/images/icons/logo.png" alt="logo"></li>
+    </ul>  
+
+  
+
+    <ul class="mainLogReg">
+        <li><a href="">Login |</a></li>
+        <li><a href="">Register</a></li>
+
+    </ul>    
+
+     <ul class="mainNavItems">
+        <li><a href="">Home</a></li>
+        <li><a href="">Browse Movies</a></li>
+
+    </ul>  
+
+    <ul class="mainSearch">
+       <input type="search">
+    </ul>  
+</nav>
+ 
+
 
     <main>
-    @yield('content')
+        @yield('content')
     </main>
 
+    <div class="footer">
+    Movie Site &copy; - 2018
+    </div>
 
+
+    <script src="http://localhost:8000/js/swiper.min.js"></script>
+   <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  </script>
 
 </body>
 </html>
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,6 +2,84 @@
 
 
 @section ('content')
-<h1>Movies Index </h1>
 
+<div class="indexTitle"> Popular Movies</div>
+ <!-- Swiper -->
+ <div class="swiper-container">
+    <div class="swiper-wrapper">
+
+      <div class="swiper-slide">
+          <div class="imgBx">
+            <img src="http://localhost:8000/images/movies/prestige.jpg" id="slideImage" alt="s1">
+        </div>
+        <div class="details">
+            The Prestige <br>
+            2006
+        </div>
+      </div>
+
+      <div class="swiper-slide">
+          <div class="imgBx">
+            <img src="http://localhost:8000/images/movies/avatar.jpg" id="slideImage" alt="s1">
+        </div>
+        <div class="details">
+           Avatar <br>
+           2009
+        </div>
+      </div>
+
+      <div class="swiper-slide">
+          <div class="imgBx">
+            <img src="http://localhost:8000/images/movies/wonderful.jpg" id="slideImage" alt="s1">
+        </div>
+        <div class="details">
+            It's Wonderful Life <br>
+            1946
+        </div>
+      </div>
+   
+    </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+</div>
+
+<div class="indexTitle"> Latest Movies</div>
+ <!-- Swiper -->
+ <div class="swiper-container">
+    <div class="swiper-wrapper">
+
+      <div class="swiper-slide">
+          <div class="imgBx">
+            <img src="http://localhost:8000/images/movies/prestige.jpg" id="slideImage" alt="s1">
+        </div>
+        <div class="details">
+            The Prestige <br>
+            2006
+        </div>
+      </div>
+
+      <div class="swiper-slide">
+          <div class="imgBx">
+            <img src="http://localhost:8000/images/movies/avatar.jpg" id="slideImage" alt="s1">
+        </div>
+        <div class="details">
+           Avatar <br>
+           2009
+        </div>
+      </div>
+
+      <div class="swiper-slide">
+          <div class="imgBx">
+            <img src="http://localhost:8000/images/movies/wonderful.jpg" id="slideImage" alt="s1">
+        </div>
+        <div class="details">
+            It's Wonderful Life <br>
+            1946
+        </div>
+      </div>
+   
+    </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+</div>
 @endsection
